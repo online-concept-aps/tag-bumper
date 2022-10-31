0c10c4542ba80064269afa372d83636551a099b2
Klaus Pedersen <klaus.pedersen@onlineconcept.dk>
2022-10-31 16:18:44 +0100

    javascript runner

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,9 @@
 const core = require( '@actions/core' );
-
-var username = core.getInput( 'username' );
-
-console.log(`Hello ${username}`)
+const github = require('@actions/github');
+const payload = JSON.stringify(github.context.payload, undefined, 2);
+const version = "1.0.0";
+var prefix = core.getInput( 'prefix-tag' );
+var token = core.getInput( 'github-token' );
+console.log(`The event payload: ${payload}`);
+console.log(`Hello ${prefix}`)
+console.log(`::set-output name=myOutput::${prefix +"-"+ version}`)
