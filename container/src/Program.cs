905ffcb31b72f4475d1fd0a4254f499a4a7e4339
Klaus Pedersen <klaus.pedersen@onlineconcept.dk>
2022-10-31 15:11:30 +0100

    go ham

--- container/src/Program.cs
+++ container/src/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine($"Hello {Environment.GetCommandLineArgs()[1]}");
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Hello {Environment.GetEnvironmentVariable("username")}!");
