3b68b87485860d9d88445c5486045ccd22a4f6de
Klaus Pedersen <klaus.pedersen@onlineconcept.dk>
2022-10-31 23:15:32 +0100

    TS

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-const core = require( '@actions/core' );
-const github = require('@actions/github');
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-const version = "1.0.0";
-var prefix = core.getInput( 'prefix-tag' );
-var token = core.getInput( 'github-token' );
-console.log(`The event payload: ${payload}`);
-console.log(`Hello ${prefix}`)
-core.setOutput( "new-tag", version );
---
+++ javascript/src/index.ts
@@ -0,0 +1,16 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+
+const payload = JSON.stringify(github.context.payload, undefined, 2);
+const version = "1.0.0";
+
+
+var prefix = core.getInput( 'prefix-tag' );
+var token = core.getInput( 'github-token' );
+
+
+console.log(`The event payload: ${payload}`);
+
+
+core.setOutput( "new-tag", version );
