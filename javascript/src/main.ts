5c66b9a16d57670aeb94925947de0c4f73be1b75
Klaus Pedersen <klaus.pedersen@gls-denmark.com>
2022-11-01 08:04:36 +0100

    Logs

--- javascript/src/main.ts
+++ javascript/src/main.ts
@@ -4,12 +4,13 @@ import * as github from '@actions/github';
 
 const payload = JSON.stringify(github.context.payload, undefined, 2);
 const version = "1.0.0";
-
+const commitSha = core.getInput("commit_sha");
+const { GITHUB_REF, GITHUB_SHA } = process.env;
 
 var prefix = core.getInput( 'prefix-tag' );
 var token =  process.env.GITHUB_TOKEN;
 
-
+console.log(GITHUB_REF)
 console.log(`The event payload: ${payload}`);
 
 
