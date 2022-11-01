2d66971e33dac91d9804a3532c0cb4ec0baa0ca3
Klaus Pedersen <klaus.pedersen@gls-denmark.com>
2022-11-01 11:04:44 +0100

    maybe

--- javascript/lib/main.js
+++ javascript/lib/main.js
@@ -2,25 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const octokit = require('@octokit/rest');
 
+async function run(){
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const repository = github.context.payload.repository;
+    console.log("Payload: " + payload);
+    const version = "1.0.0";
+    console.log("version: " + version);
+    var token =  process.env.GITHUB_TOKEN;
+    console.log("token: " +token)
+    const { GITHUB_REF, GITHUB_SHA } = process.env;
+    console.log("GITHUB_REF: " +GITHUB_REF)
+    console.log("GITHUB_SHA: " +GITHUB_SHA)
+    console.log(JSON.stringify(github.context.repo))
+    var client = github.getOctokit(token);
 
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-const repository = github.context.payload.repository;
-console.log("Payload: " + payload);
-const version = "1.0.0";
-console.log("version: " + version);
-var token =  process.env.GITHUB_TOKEN;
-console.log("token: " +token)
-const { GITHUB_REF, GITHUB_SHA } = process.env;
-console.log("GITHUB_REF: " +GITHUB_REF)
-console.log("GITHUB_SHA: " +GITHUB_SHA)
-console.log(JSON.stringify(github.context.repo))
-var client = new octokit.Octokit({
-    auth: token
-});
-client.git.createTag({
+
+
+    const tag_resp = await client.rest.git.createTag({
     ...github.context.repo,
     tag: version,
     message:version,
     object: github.context.sha,
     type: 'commit'
-})
+    })
+    console.log(tag_resp);
+}
+run();
