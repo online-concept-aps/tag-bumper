42a54f6f813f4156cd23bdfe94e696df7e63e24e
Klaus Pedersen <klaus.pedersen@gls-denmark.com>
2022-11-01 12:11:18 +0100

    maybe

--- javascript/lib/main.js
+++ javascript/lib/main.js
@@ -6,7 +6,7 @@ async function run(){
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     const repository = github.context.payload.repository;
     //console.log("Payload: " + payload);
-    const version = "1.0.0";
+    const version = "test-1.0.0";
     //console.log("version: " + version);
     var token =  process.env.GITHUB_TOKEN;
     //console.log("token: " +token)
