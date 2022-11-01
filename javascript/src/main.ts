f573d8e3a432fe87b9381223d80a758c707f156e
Klaus Pedersen <klaus.pedersen@gls-denmark.com>
2022-11-01 09:49:56 +0100

    maybe

--- javascript/src/main.ts
+++ javascript/src/main.ts
@@ -1,17 +1,36 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { inc, parse, ReleaseType, valid, gte, SemVer } from "semver";
+import { analyzeCommits } from "@semantic-release/commit-analyzer";
+import { generateNotes } from "@semantic-release/release-notes-generator";
+import {
+    createTag,
+    getBranchFromRef
+} from "./utils";
+
+async function run(){
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const version = "1.0.0";
+    let commitSha = core.getInput("commit_sha");
+    const { GITHUB_REF, GITHUB_SHA } = process.env;
+
+    var prefix = core.getInput( 'prefix-tag' );
+    var token =  process.env.GITHUB_TOKEN;
+
+    if (!GITHUB_REF) {
+        core.setFailed("Missing GITHUB_REF.");
+        return;
+    }
 
 
-const payload = JSON.stringify(github.context.payload, undefined, 2);
-const version = "1.0.0";
-const commitSha = core.getInput("commit_sha");
-const { GITHUB_REF, GITHUB_SHA } = process.env;
+    const currentBranch = getBranchFromRef(GITHUB_REF);
+    await createTag("test-1.0.0", commitSha);
 
-var prefix = core.getInput( 'prefix-tag' );
-var token =  process.env.GITHUB_TOKEN;
-
-console.log("anything" ,GITHUB_REF,GITHUB_SHA)
-console.log(`The event payload: ${payload}`);
+    console.log("anything" ,GITHUB_REF,GITHUB_SHA)
+    console.log(`The event payload: ${payload}`);
 
 
-core.setOutput( "new-tag", version );
+    core.setOutput( "new-tag", version );
+}
+
+run();
---
+++ javascript/src/utils.ts
@@ -0,0 +1,84 @@
+import * as core from "@actions/core";
+import { Octokit } from "@octokit/rest";
+import { context } from "@actions/github";
+import * as github from '@actions/github';
+import { valid, rcompare, prerelease } from "semver";
+
+
+const githubToken = core.getInput("github_token");
+const octokit = new Octokit({
+    auth: githubToken
+});
+
+export async function getValidTags() {
+  const tags = await octokit.repos.listTags({
+    ...context.repo,
+    per_page: 100,
+  });
+
+  const invalidTags = tags.data
+    .map((tag) => tag.name)
+    .filter((name) => !valid(name));
+
+  invalidTags.forEach((name) => core.debug(`Found Invalid Tag: ${name}.`));
+
+  const validTags = tags.data
+    .filter((tag) => valid(tag.name))
+    .sort((a, b) => rcompare(a.name, b.name));
+
+  validTags.forEach((tag) => core.debug(`Found Valid Tag: ${tag.name}.`));
+
+  return validTags;
+}
+
+export async function getCommits(sha: string) {
+  const commits = await octokit.repos.compareCommits({
+    ...context.repo,
+    base: sha,
+    head: "HEAD",
+  });
+
+  return commits.data.commits
+    .filter((commit) => !!commit.commit.message)
+    .map((commit) => ({
+      message: commit.commit.message,
+      hash: commit.sha,
+    }));
+}
+
+export function getBranchFromRef(ref: string) {
+  return ref.replace("refs/heads/", "");
+}
+
+export async function createTag(
+  newTag: string,
+  commitSha: string
+) {
+
+  core.debug(`Pushing new tag to the repo.`);
+  await octokit.git.createRef({
+    ...context.repo,
+    ref: `refs/tags/${newTag}`,
+    sha: commitSha,
+  });
+}
+
+export function getLatestTag(tags: any) {
+  return (
+    tags.find((tag) => !prerelease(tag.name)) || {
+      name: "0.0.0",
+      commit: {
+        sha: "HEAD",
+      },
+    }
+  );
+}
+
+export function getLatestPrereleaseTag(
+  tags: any,
+  identifier: string
+) {
+  return tags
+    .filter((tag) => prerelease(tag.name))
+    .find((tag) => tag.name.match(identifier));
+}
